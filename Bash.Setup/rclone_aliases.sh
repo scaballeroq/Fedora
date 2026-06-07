a2b69c815d9ad163961c1039862a965083539f85
Sergio Caballero <scaballeroq@gmail.com>
2026-06-07 18:55:47 +0200

    refactor: update rclone aliases to use double quotes for improved variable expansion reliability

--- Bash.Setup/rclone_aliases.sh
+++ Bash.Setup/rclone_aliases.sh
@@ -17,22 +17,22 @@ RCLONE_OPTS="--fast-list --transfers 8 --checkers 16 --tpslimit 10 --verbose -P"
 # 3. GOOGLE DRIVE (UPLOAD) - SUBIR A LA NUBE
 # -----------------------------------------------------------------------------
 
-alias gdrive-imagenes='rclone sync "$HOME/Imágenes" "GoogleDrive:Imágenes" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_imagenes.log"'
-alias gdrive-documentos='rclone sync "$HOME/Documentos/" "GoogleDrive:Documentos" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_linuxhowto.log"'
-alias gdrive-avatar='rclone sync "$HOME/Imágenes/Avatar" "GoogleDrive:Avatar" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_avatar.log"'
-alias gdrive-videos='rclone sync "$HOME/Vídeos" "GoogleDrive:Vídeos" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_videos.log"'
-alias gdrive-musica='rclone sync "$HOME/Música" "GoogleDrive:Música" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_musica.log"'
-alias gdrive-software='rclone sync "/media/caballero/NVME_EXT/Software" "GoogleDrive:Software" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_software.log"'
+alias gdrive-imagenes="rclone sync \"\$HOME/Imágenes\" \"GoogleDrive:Imágenes\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_imagenes.log\""
+alias gdrive-documentos="rclone sync \"\$HOME/Documentos/\" \"GoogleDrive:Documentos\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_linuxhowto.log\""
+alias gdrive-avatar="rclone sync \"\$HOME/Imágenes/Avatar\" \"GoogleDrive:Avatar\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_avatar.log\""
+alias gdrive-videos="rclone sync \"\$HOME/Vídeos\" \"GoogleDrive:Vídeos\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_videos.log\""
+alias gdrive-musica="rclone sync \"\$HOME/Música\" \"GoogleDrive:Música\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_musica.log\""
+alias gdrive-software="rclone sync \"/media/caballero/NVME_EXT/Software\" \"GoogleDrive:Software\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_software.log\""
 
 # -----------------------------------------------------------------------------
 # 4. GOOGLE DRIVE (DOWNLOAD) - BAJAR DE LA NUBE
 # -----------------------------------------------------------------------------
 
-alias gdrive-imagenes-down='rclone sync "GoogleDrive:Imágenes" "$HOME/Imágenes" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_imagenes_down.log"'
-alias gdrive-documentos-down='rclone sync "GoogleDrive:Documentos" "$HOME/Documentos/" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_linuxhowto_down.log"'
-alias gdrive-avatar-down='rclone sync "GoogleDrive:Avatar" "$HOME/Imágenes/Avatar" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_avatar_down.log"'
-alias gdrive-videos-down='rclone sync "GoogleDrive:Vídeos" "$HOME/Vídeos" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_videos_down.log"'
-alias gdrive-musica-down='rclone sync "GoogleDrive:Música" "$HOME/Música" $RCLONE_OPTS --log-file "$RCLONE_LOG_DIR/rclone_musica_down.log"'
+alias gdrive-imagenes-down="rclone sync \"GoogleDrive:Imágenes\" \"\$HOME/Imágenes\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_imagenes_down.log\""
+alias gdrive-documentos-down="rclone sync \"GoogleDrive:Documentos\" \"\$HOME/Documentos/\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_linuxhowto_down.log\""
+alias gdrive-avatar-down="rclone sync \"GoogleDrive:Avatar\" \"\$HOME/Imágenes/Avatar\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_avatar_down.log\""
+alias gdrive-videos-down="rclone sync \"GoogleDrive:Vídeos\" \"\$HOME/Vídeos\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_videos_down.log\""
+alias gdrive-musica-down="rclone sync \"GoogleDrive:Música\" \"\$HOME/Música\" $RCLONE_OPTS --log-file \"$RCLONE_LOG_DIR/rclone_musica_down.log\""
 
 # 5. Limpieza de variables temporales para evitar contaminar la shell
 unset RCLONE_LOG_DIR
